63074c313395fdb21c5ac8ad7897383ccebde842
Григорий Васильчук <grisha.vasilchuk.200201@gmail.com>
2024-05-10 19:34:08 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext _dbContext;
+
+        public OrderService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var order = await _dbContext.Orders
+                .Where(o => o.Quantity > 1)
+                .OrderByDescending(o => o.CreatedAt)
+                .FirstOrDefaultAsync();
+
+            if (order == null)
+            {
+                throw new ArgumentException("Order does not exist!");
+            }
+
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var orders = await _dbContext.Orders
+                .Where(o => o.User.Status == UserStatus.Active)
+                .OrderBy(o => o.CreatedAt)
+                .ToListAsync();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext _dbContext;
+
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var user = await _dbContext.Users
+                .OrderByDescending(u => u.Orders.Where(o => o.CreatedAt.Year == 2003).Sum(o => o.Price))
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                throw new ArgumentException("User does not exist!");
+            }
+
+            return user;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users = await _dbContext.Users
+                .Include(u => u.Orders)
+                .Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2010 && o.Status == OrderStatus.Paid))
+                .ToListAsync();
+
+            return users;
+        }
+    }
+}
